ba3c2d7c4950c551b1a56700b3a224649e2aad05
Aliyu Bello <aliyubababello@gmail.com>
2026-01-04 07:21:25 +0100

    whatsapp webhook added-1

--- dist/src/config/db.js
+++ dist/src/config/db.js
@@ -1,26 +1,43 @@
 "use strict";
-// import { Sequelize } from 'sequelize';
-// import dotenv from 'dotenv';
-// dotenv.config();
+var __importDefault = (this && this.__importDefault) || function (mod) {
+    return (mod && mod.__esModule) ? mod : { "default": mod };
+};
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.sequelize = void 0;
-// const connectionString = process.env.DATABASE_URL as string;
-// export const sequelize = new Sequelize(connectionString, {
-//   dialect: 'postgres',
-//   logging: false,
-// });
-// export default sequelize;
 const sequelize_1 = require("sequelize");
-const sequelize = new sequelize_1.Sequelize(process.env.DB_NAME || 'fintech', process.env.DB_USER || 'postgres', process.env.DB_PASSWORD || 'rexali', {
-    host: process.env.DB_HOST || 'localhost',
-    port: parseInt(process.env.DB_PORT || '5432'),
-    dialect: 'postgres',
-    logging: process.env.NODE_ENV === 'development' ? console.log : false,
-    pool: {
-        max: 10,
-        min: 0,
-        acquire: 30000,
-        idle: 10000
+const dotenv_1 = __importDefault(require("dotenv"));
+dotenv_1.default.config();
+function getSequelizeInstance() {
+    let database;
+    let username;
+    let password;
+    let host;
+    let dialect;
+    if (process.env.NODE_ENV !== "development") {
+        database = process.env.PROD_DB_NAME, //config.database_prod;
+            username = process.env.PROD_DB_USER, // config.username_prod;
+            password = process.env.PROD_DB_PASS; // config.password_prod;
+        host = process.env.PROD_DB_HOST; // config.host_prod;
+        dialect = 'postgres'; // config.dialect_prod;
     }
-});
+    else {
+        database = process.env.DB_NAME;
+        username = process.env.DB_USER;
+        password = process.env.DB_PASS;
+        host = process.env.DB_HOST;
+        dialect = 'postgres';
+    }
+    let sequelize = new sequelize_1.Sequelize(database, username, password, {
+        host,
+        dialect,
+        pool: {
+            max: 15,
+            min: 0,
+            acquire: 30000,
+            idle: 10000
+        }
+    });
+    return sequelize;
+}
+let sequelize = getSequelizeInstance();
 exports.sequelize = sequelize;
--- src/config/db.ts
+++ src/config/db.ts
@@ -1,34 +1,51 @@
 
-// import { Sequelize } from 'sequelize';
-// import dotenv from 'dotenv';
-// dotenv.config();
+import { Sequelize, } from 'sequelize';
+import dotenv from 'dotenv';
+dotenv.config();
 
-// const connectionString = process.env.DATABASE_URL as string;
-// export const sequelize = new Sequelize(connectionString, {
-//   dialect: 'postgres',
-//   logging: false,
-// });
+function getSequelizeInstance(){
 
-// export default sequelize;
+    let database:string;
+    let username:string;
+    let password:string; 
+    let host:string;
+    let dialect:any;
 
-import { Sequelize } from 'sequelize';
-
-const sequelize = new Sequelize(
-  process.env.DB_NAME || 'fintech',
-  process.env.DB_USER || 'postgres',
-  process.env.DB_PASSWORD || 'rexali',
-  {
-    host: process.env.DB_HOST || 'localhost',
-    port: parseInt(process.env.DB_PORT || '5432'),
-    dialect: 'postgres',
-    logging: process.env.NODE_ENV === 'development' ? console.log : false,
-    pool: {
-      max: 10,
-      min: 0,
-      acquire: 30000,
-      idle: 10000
+       if (process.env.NODE_ENV !== "development") {
+        database =process.env.PROD_DB_NAME as string, //config.database_prod;
+        username = process.env.PROD_DB_USER as string, // config.username_prod;
+        password = process.env.PROD_DB_PASS as string; // config.password_prod;
+        host = process.env.PROD_DB_HOST as string; // config.host_prod;
+        dialect = 'postgres' as any; // config.dialect_prod;
+    } else {
+        database = process.env.DB_NAME as string;
+        username = process.env.DB_USER as string;
+        password = process.env.DB_PASS as string;
+        host = process.env.DB_HOST as string
+        dialect = 'postgres' as any;
     }
-  }
-);
+
+    let sequelize = new Sequelize(
+        database,
+        username,
+        password,
+        {
+            host,
+            dialect,
+            pool: {
+                max: 15,
+                min: 0,
+                acquire: 30000,
+                idle: 10000
+            }
+        });
+   
+
+    
+
+    return sequelize;
+}
+
+let sequelize = getSequelizeInstance();
 
 export { sequelize };
